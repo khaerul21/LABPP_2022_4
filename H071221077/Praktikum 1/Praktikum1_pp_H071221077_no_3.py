44f9ae8d3314d92b7595512b0c022bea92a022f3
Fadillah Cheryl <fadillahcheryl@serls-MacBook-Air.local>
2022-09-18 20:33:22 +0800

    uplod tugas ke-1

---
+++ H071221077/Praktikum 1/Praktikum1_pp_H071221077_no_3.py
@@ -0,0 +1,9 @@
+
+print("Menghitung Jumlah Tagihan Listrik Bulanan")
+
+rata_rata = float(input("Masukkan Rata-rata : "))
+harga = 1325
+hari = 30
+
+total = rata_rata * harga * hari
+print("Jumlah tagihan listrik bulanan : Rp. %.02f" % total)
