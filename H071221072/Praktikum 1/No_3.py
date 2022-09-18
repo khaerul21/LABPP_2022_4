0c8f3fc221918ed744992079acc0774613675b27
Orii21 <hoirimiftahul21@gmail.com>
2022-09-17 19:01:39 -0700

    Praktikum 1

---
+++ H071221072/Praktikum 1/No_3.py
@@ -0,0 +1,9 @@
+'''Mifthahul Hoiri Bachrudin Basir
+    H071221072'''
+#Menghitung persenan gaji
+print(input('Masukkan Nama: '))
+a = float(input('Gaji pokok: '))
+b = float(input('Total penjualan: '))
+c = (b * 0.15)
+d = round(a + c,2)
+print('TOTAL = $ ',d)
