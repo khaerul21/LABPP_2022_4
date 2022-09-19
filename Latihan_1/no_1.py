1cc80467a65f9df2837b87d94749bd404809b908
Rafliiii <113847001+Rafliiii@users.noreply.github.com>
2022-09-19 11:34:28 +0800

    Add files via upload

---
+++ Latihan_1/no_1.py
@@ -0,0 +1,23 @@
+#Menghitung panjang kapal 
+import math
+from re import A
+
+h = int(input('h: '))
+#a = int(input('a: '))
+#b = int(input('b: '))
+#rad = (math.pi/180)*a 
+#hasiltan = math.tan(rad)
+# x = h / round(hasiltan,1)
+
+a = int(input('a: '))
+rad = (math.pi/180)*a
+x = math.tan(rad)
+# x = float(hasilTan1)
+
+b = int(input('b: '))
+rad = (math.pi/180)*b
+y = math.tan(rad)
+# y = float(hasilTan2)
+
+hasil = round(h * x - h * y,1)
+print(hasil,'m')
